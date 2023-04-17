8dbe44b9177696b4e03219a5b52b91563bb62eb5
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-04-17 15:40:36 -0400

    Modify this code to allow for multiple users that log in with a username and password. Also, add code to handle executing orders when they cross, and print out that the order executed with the relevant information.  Give every user a balance of  and 0 stocks to start and update user's balance and stocks whenever a trade executes.

--- limit_order_book.py
+++ limit_order_book.py
@@ -3,7 +3,8 @@ from collections import deque
 from datetime import datetime
 
 class Order:
-    def __init__(self, price, quantity, timestamp):
+    def __init__(self, user, price, quantity, timestamp):
+        self.user = user
         self.price = price
         self.quantity = quantity
         self.timestamp = timestamp
@@ -15,40 +16,66 @@ class LimitOrderBook:
         heapq.heapify(self.bids)
         heapq.heapify(self.asks)
 
-    def add_order(self, side, price, quantity):
-        order = Order(price, quantity, datetime.now())
+    def add_order(self, side, price, quantity, user):
+        order = Order(user, price, quantity, datetime.now())
         if side == 'bid':
-            # If there's a tie with the first element of the tuple, then heapq looks at the second element of the tuple to break the tie, and so on.
-            # by default, Python heapq implements a min heap. So, need -price to get a max heap.
-            # So, this achieves price-time priority for bids and asks.
             heapq.heappush(self.bids, (-price, order.timestamp, order))
         elif side == 'ask':
             heapq.heappush(self.asks, (price, order.timestamp, order))
+        self.match_orders()
         self.display()
 
-    def cancel_order(self, side, price):
+    def cancel_order(self, side, price, user):
         if side == 'bid':
             target_book = self.bids
         elif side == 'ask':
             target_book = self.asks
 
         for i in range(len(target_book)):
-            if target_book[i][2].price == price:
+            if target_book[i][2].price == price and target_book[i][2].user == user:
                 del target_book[i]
                 heapq.heapify(target_book)
-                
                 self.display()
                 return True
         return False
 
+    def match_orders(self):
+        while self.bids and self.asks:
+            bid = self.bids[0][2]
+            ask = self.asks[0][2]
+
+            if -self.bids[0][0] >= self.asks[0][0]:  # Check if top bid price is >= top ask price
+                executed_quantity = min(bid.quantity, ask.quantity)
+                execution_price = (bid.price + ask.price) / 2
+
+                bid.user.balance += -executed_quantity * execution_price
+                bid.user.stocks += executed_quantity
+                ask.user.balance += executed_quantity * execution_price
+                ask.user.stocks += -executed_quantity
+
+                bid.quantity -= executed_quantity
+                ask.quantity -= executed_quantity
+
+                if bid.quantity == 0:
+                    heapq.heappop(self.bids)
+                if ask.quantity == 0:
+                    heapq.heappop(self.asks)
+
+                print(f"\nOrder executed: {executed_quantity} shares at ${execution_price:.2f}")
+                print(f"{bid.user.username} new balance: ${bid.user.balance:.2f}, stocks: {bid.user.stocks}")
+                print(f"{ask.user.username} new balance: ${ask.user.balance:.2f}, stocks: {ask.user.stocks}")
+
+            else:
+                break
 
     def display(self):
         print("Bids:")
         for bid in self.bids:
-            print(f"Price: {bid[2].price}, Quantity: {bid[2].quantity}, Timestamp: {bid[1]}")
+            print(f"User: {bid[2].user.username}, Price: {bid[2].price}, Quantity: {bid[2].quantity}, Timestamp: {bid[1]}")
         print("\nAsks:")
         for ask in self.asks:
-            print(f"Price: {ask[2].price}, Quantity: {ask[2].quantity}, Timestamp: {ask[1]}")
+            print(f"User: {ask[2].user.username}, Price: {ask[2].price}, Quantity: {ask[2].quantity}, Timestamp: {ask[1]}")
+
 
 def main():
     book = LimitOrderBook()
